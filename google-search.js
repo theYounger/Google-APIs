e3477508d15442feab5ba6f71a87bca7e9efa890
kyle uyehara <kuyehara1@gmail.com>
2017-04-04 13:49:46 -1000

    Node's http example for sending requests to server.

--- google-search.js
+++ google-search.js
@@ -1,2 +1,24 @@
 var http = require("http");
 
+function redditRes(res) {
+  res.on("data", function resStream(chunk) {
+    console.log(`in ${this}`);
+    console.log(`BODY: ${chunk}`);
+  });
+  res.on("end", function resEnd() {
+    console.log("No more data in response.");
+  });
+}
+
+var options = {
+  host: "www.reddit.com",
+  path: "/r/The_Donald/.json",
+  method: "GET",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  }
+};
+
+var req = http.request(options, redditRes);
+
+req.end();
